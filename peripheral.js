bed2d78198c591e09924f94663b155d1cd59c5ad
amit chahar <amitchahar_12277@aitpune.edu.in>
2016-08-12 16:18:05 +0530

    version characteristic added

--- peripheral.js
+++ peripheral.js
@@ -9,7 +9,10 @@ var name = 'commService';
 var commServiceUuid = '12ab';
 var commCharacteristicUuid = '34cd';
 var commCharacteristicUuid2 = '56ef';
+var FT_CONTROL_FVERSION_UUID = '78GH';
+
 var logfile_name = __dirname + '/firmware_file';
+var fversion_file = __dirname + '/fversion';
 
 function delete_file(filename) {
     fs.stat(filename, function (err, stat) {
@@ -22,6 +25,16 @@ function delete_file(filename) {
             console.log("error while deleting file: " + filename);
         }
     });
+}
+
+function write_firmware_version(value){
+	fs.writeFileSync(fversion_file, value);
+}
+
+function read_existing_firmware_version(){
+	var version = fs.readFileSync(fversion_file);
+	console.log("current version of firmware : " + version.trim());
+	return version.trim();
 }
 
 var first_write = true;
@@ -61,6 +74,28 @@ var service1 = new bleno.PrimaryService({
                 //this.value = data;
                 //console.log('Write request: value = ' + this.value.toString("utf-8"));
                 write_to_file(data.toString('utf-8'));
+                callback(this.RESULT_SUCCESS);
+            }
+
+        }),
+        new bleno.Characteristic({
+            value: null,
+            uuid: FT_CONTROL_FVERSION_UUID,
+            properties: ['writeWithoutResponse', 'notify', 'read', 'write'],
+            secure: ['read', 'write', 'writeWithoutResponse'],
+
+            // Send a message back to the client with the characteristic's value
+            onReadRequest: function (offset, callback) {
+            	var version = read_existing_firmware_version();
+                console.log("Read request received");
+                callback(this.RESULT_SUCCESS, new Buffer(version));
+            },
+
+            // Accept a new value for the characterstic's value
+            onWriteRequest: function (data, offset, withoutResponse, callback) {
+                //this.value = data;
+                //console.log('Write request: value = ' + this.value.toString("utf-8"));
+                write_firmware_version(data.toString('utf-8'));
                 callback(this.RESULT_SUCCESS);
             }
 
